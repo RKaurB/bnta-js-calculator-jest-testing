5daf44665a39173e7cd9391f3ca8da9126ea7949
Rachel Kaur <rachel.km@outlook.com>
2022-03-08 23:32:24 +0000

    complete all functions and add tests for add function

--- calculator.js
+++ calculator.js
@@ -1,18 +1,72 @@
-const sum = function(a, b){
-    return a + b;
+// (Anonymous) sum function - sums two numbers
+const sum = function(num1, num2) {
+    return num1 + num2;
 };
+// Call function
+console.log(sum(1, 3));
 
-const subtract = null;
+// (Arrow) sum function
+// const sumArrow = (num1, num2) => num1 + num2;
+// console.log(sumArrow(1, 3));
 
-const multiply = null;
 
-const divide = null;
+// Subtract function - subtracts second number from first number
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+// Call function
+console.log(subtract(7, 2));
 
-const modulus = null;
 
-const even = null;
+// Multiply function - multiplies two numbers
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+// Call function
+console.log(multiply(2,10));
 
-const odd = null;
+
+// Divide function - divides first number by second number
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+// Call function
+console.log(divide(5, 2));
+
+
+// Modulus function - returns remainder of first number divided by second number
+const modulus = function(num1, num2) {
+    return num1 % num2;
+};
+// Call function
+console.log(modulus(3, 2));
+
+
+// Even function - returns true if number is even, false if not even
+const even = function(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+// Call function
+console.log(even(8));
+console.log(even(7));
+
+
+// Odd function - returns true if number is odd, false if not odd
+const odd = function(num) {
+    if (num % 2 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+// Call function
+console.log(odd(8));
+console.log(odd(7));
+
 
 module.exports = { 
     sum, 
--- calculator.test.js
+++ calculator.test.js
@@ -8,30 +8,47 @@ const {
   odd,
 } = require("./calculator");
 
+
+// Describe block - testing sum function
 describe('sum', () => { 
-  
+
   test('can add two small positive numbers', () => {
     expected = 5;
     actual = sum(2, 3);
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  // test.skip('can add two large positive numbers', () => {
+  test('can add two large positive numbers', () => { 
+    expected = 968_000_000_000_000;
+    actual = sum(500_000_000_000_000, 468_000_000_000_000);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  // test.skip('can add two negative numbers', () => {
+    test('can add two negative numbers', () => {
+    expected = -8;
+    actual = sum(-5, -3);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  // test.skip('can add zero', () => {
+  test('can add zero', () => {
+    expected = 28;
+    actual = sum(28, 0);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add decimal numbers', () => {
+    expected = 28.3;
+    actual = sum(15.1, 13.2);
+    expect(actual).toBeCloseTo(expected);
   });
 
 });
 
-describe('subtract', () => {
 
+describe('subtract', () => {
 });
 
 describe('multiply', () => {
